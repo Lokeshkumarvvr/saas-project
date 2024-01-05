46b83c96337494a846894b51f358df0d81198947
Lokeshkumarvvr <lokeshvvr17@gmail.com>
2024-01-05 12:51:56 +0530

    babel adder 1

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,22 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+// webpack/environment.js or webpack/production.js
 
-const environment = require('./environment')
+const { environment } = require('@rails/webpacker');
 
-module.exports = environment.toWebpackConfig()
+const babelLoader = {
+  test: /\.m?js$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: ['@babel/preset-env']
+      // Add other Babel options or plugins here if needed
+    }
+  }
+};
+
+// Add Babel loader to environment if it's defined
+if (environment && environment.loaders) {
+  environment.loaders.append('babel', babelLoader);
+}
+
+module.exports = environment;
