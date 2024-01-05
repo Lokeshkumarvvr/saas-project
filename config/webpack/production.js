e0e13306d598355f7a35a831b455a64a0410829e
Lokeshkumarvvr <lokeshvvr17@gmail.com>
2024-01-05 11:44:08 +0530

    production.js

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,13 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
-const environment = require('./environment')
+const { environment } = require('./environment');
+const babelLoader = environment.loaders.get('babel');
 
-module.exports = environment.toWebpackConfig()
+if (babelLoader) {
+  // Exclude specific dependencies from Babel transpilation in production
+  babelLoader.exclude = /node_modules\/(?!your-dependency-to-exclude)/;
+  // Replace 'your-dependency-to-exclude' with the actual dependencies you want to exclude
+  // Adjust the regex pattern to match the specific dependencies
+}
+
+module.exports = environment.toWebpackConfig();
