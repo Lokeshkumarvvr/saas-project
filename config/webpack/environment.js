208c20a1e1dcd6afb2b6b473bc8cb9ebad746bc8
Lokeshkumarvvr <lokeshvvr17@gmail.com>
2024-01-05 11:58:38 +0530

    webpacker manofest

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,3 @@
-const { environment } = require('@rails/webpacker');
-const babelLoader = environment.loaders.get('babel');
-babelLoader.exclude = /node_modules\/(?!your-dependency-to-exclude)/; // Replace 'your-dependency-to-exclude' with actual dependencies if needed
-module.exports = environment;
+const { environment } = require('@rails/webpacker')
+
+module.exports = environment
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,13 +1,5 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const { environment } = require('./environment');
-const babelLoader = environment.loaders.get('babel');
+const environment = require('./environment')
 
-if (babelLoader) {
-  // Exclude specific dependencies from Babel transpilation in production
-  babelLoader.exclude = /node_modules\/(?!your-dependency-to-exclude)/;
-  // Replace 'your-dependency-to-exclude' with the actual dependencies you want to exclude
-  // Adjust the regex pattern to match the specific dependencies
-}
-
-module.exports = environment.toWebpackConfig();
+module.exports = environment.toWebpackConfig()
