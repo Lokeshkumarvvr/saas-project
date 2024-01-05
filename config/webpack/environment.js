c93cf84d12fdd616dafdd9ad7e71d2a7cced151a
Lokeshkumarvvr <lokeshvvr17@gmail.com>
2024-01-05 12:03:53 +0530

    babel error

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,4 @@
 const { environment } = require('@rails/webpacker')
-
+const babelLoader = environment.loaders.get('babel');
+         babelLoader.exclude = /node_modules\/(?!your-dependency-to-exclude)/; // Replace 'your-dependency-to-exclude' with actual dependencies if needed
 module.exports = environment
