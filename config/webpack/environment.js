359932c16ffdac621131a02c8a970259e0159a1a
Lokeshkumarvvr <lokeshvvr17@gmail.com>
2024-01-05 12:47:57 +0530

    add babel adder

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,6 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
+
+// Your other webpack configurations here...
 const babelLoader = environment.loaders.get('babel');
-         babelLoader.exclude = /node_modules\/(?!your-dependency-to-exclude)/; // Replace 'your-dependency-to-exclude' with actual dependencies if needed
-module.exports = environment
+babelLoader.exclude = /node_modules\/(?!your-dependency-to-exclude)/; // Replace 'your-dependency-to-exclude' with actual dependencies if needed
+module.exports = environment;
